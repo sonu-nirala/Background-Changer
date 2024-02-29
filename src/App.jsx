75930cbd3dfc58d301d56c68dca94cbb7ef8937f
Sonu <sonublh2021@gmail.com>
2024-02-29 21:19:32 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,51 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [color, setColor] = useState("indigo")
+
+  return (
+    <div className="w-full h-screen duration-100"
+      style={{ backgroundColor: color }}
+    >
+      <div className="fixed flex flex-wrap justify-center
+      bottom-12 inset-x-0 px-5 ">
+        <div className="flex flex-wrap justify-center
+        gap-5 shadow-lg bg-white px-4 py-2 rounded-3xl">
+
+          <button
+            onClick={() => setColor("red")}
+            className="outline-none px-4 py-2
+          rounded-full text-white shadow-lg"
+            style={{ backgroundColor: "red" }}
+          >Red</button>
+          <button
+            onClick={() => setColor("pink")}
+            className="outline-none px-4 py-2
+          rounded-full text-white shadow-lg"
+            style={{ backgroundColor: "pink" }}
+          >pink</button>
+          <button
+            onClick={() => setColor("green")}
+            className="outline-none px-4 py-2
+          rounded-full text-white shadow-lg"
+            style={{ backgroundColor: "green" }}
+          >green</button>
+          <button
+            onClick={() => setColor("orange")}
+            className="outline-none px-4 py-2
+          rounded-full text-white shadow-8xl"
+            style={{ backgroundColor: "orange" }}
+          >orange</button>
+
+
+        </div>
+      </div>
+
+    </div>
+  )
+}
+
+export default App
